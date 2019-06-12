cccb9004af2194622f2218658766d85533892911
INAMOS <edwardhurtado@live.com>
2019-06-11 22:52:10 -0400

    adding bustra

--- index.php
+++ index.php
@@ -20,6 +20,9 @@
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="vistas/css/sigin.css">
+    <link rel="stylesheet" href="vistas/css/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vistas/css/bootstrap-4.3.1-dist//4.3.1/js/bootstrap.min.js">
+    
      <!-- Page level plugin CSS-->
      
   <!-- Custom fonts for this template-->
